f254774b13538221eeaa7336effcb927d95b4bf2
tsemach <tsemach.mizrachi>
2018-08-12 00:28:28 +0300

    add component as different file

--- decorators/component.ts
+++ decorators/component.ts
@@ -1,18 +1,4 @@
-/**
- * <description>
- * </description>
- * 
- * <output>
- * class_1 {
- *  property: 'property',
- *  hello: 'override',
- *  newProperty: 'new property' }
- * </output>
- */  
-
- import { Component } from './component';
- 
-function Component2(config) {
+export function Component(config) {
   return function(target) {
     console.log('[Component:constructor] target = ' + target);
     console.log('[Component:constructor] config = ' + JSON.stringify(config));
@@ -39,31 +25,3 @@ function Component2(config) {
     console.log("In component: after definePropert " + target);
   }
 }
-
-@Component({
-  tasks: 'tasks',
-  undos: 'undos'
-})
-class Greeter {
-  property = "property";
-  hello: string;
-  constructor(m: string) {
-      console.log("[Greeter:constructor] in origin constructor");
-      this.hello = m;
-  }
-
-  tasks(data) {
-    console.log('[Greeter:tasks] is called, data = ' + JSON.stringify(data));
-  }
-
-  undos(data) {
-    console.log('[Greeter:undos] is called, data = ' + JSON.stringify(data));
-  }
-}
-
-let g = new Greeter("world")
-console.log("after ner GReeter " + JSON.stringify(g));
-
-g.sayHello({method: 'create', data: {value: 'some data here'}});
-
-//g.tasks({method: 'create', data: {value: 'some data here'}});
